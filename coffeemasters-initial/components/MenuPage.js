81424a4fa1953c17183e001f47701bf1625e452e
Patryk <patrykkonior4@gmail.com>
2023-11-04 11:44:49 +0100

    Render Menu

--- coffeemasters-initial/components/MenuPage.js
+++ coffeemasters-initial/components/MenuPage.js
@@ -7,7 +7,7 @@ export class MenuPage extends HTMLElement {
     this.root.appendChild(styles);
 
     async function LoadCSS() {
-      const request = await fetch("/components/MenuPage.css");
+      const request = await fetch("./components/MenuPage.css");
       const css = await request.text();
       styles.textContent = css;
     }
@@ -18,6 +18,34 @@ export class MenuPage extends HTMLElement {
     const template = document.getElementById("menu-page-template");
     const content = template.content.cloneNode(true);
     this.root.appendChild(content);
+
+    window.addEventListener("appmenuchange", () => {
+      this.render();
+    });
+  }
+
+  render() {
+    if (app.store.menu) {
+      this.root.querySelector("#menu").innerHTML = "";
+      for (let category of app.store.menu) {
+        const LiCategory = document.createElement("li");
+        LiCategory.innerHTML = `
+        <h3>${category.name}</h3>
+        <ul class='category'>
+
+        </ul>
+        `;
+        this.root.querySelector("#menu").appendChild(LiCategory);
+
+        category.products.forEach((product) => {
+          const item = document.createElement("product-item");
+          item.dataset.product = JSON.stringify(product);
+          LiCategory.querySelector("ul").appendChild(item);
+        });
+      }
+    } else {
+      this.root.querySelector("#menu").innerHTML = "Loading...";
+    }
   }
 }
 
